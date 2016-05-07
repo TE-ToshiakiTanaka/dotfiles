6701d61d5ed647ceb68afe22eaf241081884c27e
Toshiaki.Tanaka <Toshiaki.Tanaka@jp.sony.com>
2016-05-08 08:38:13 +0900

    update.

--- sh/init.sh
+++ sh/init.sh
@@ -4,9 +4,8 @@ if [ "$(uname)" == 'Darwin' ]; then
     echo "Darwin"
 
 elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
-    # install nodebrew.
-    sudo apt-get -y update
-    sudo apt-get -y upgrade
+    # sudo apt-get -y update
+    # sudo apt-get -y upgrade
     sudo apt-get -y install build-essential curl git openssh-server checkinstall libcurl4-openssl-dev
 
 elif [ "$(expr substr $(uname -s) 1 10)" == 'MINGW64_NT' ]; then
